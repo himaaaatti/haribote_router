6a6088fad04d55c4f8a1e7171738c04bd3c13c51
himaaaatti <sux2mfgj@gmail.com>
2015-10-19 11:52:51 +0900

    start

---
+++ ltest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <linux/if.h>
+
+
+/*
+ * args:
+ *   - const char* device   : name of network interface
+ *   - bool promisc_flag     : flag of promisc mode.
+ *   - bool ip_only          : if flagged, this function read ip packet only.
+ *
+ *
+ */
+int init_raw_socket(const char* device, bool promisc_flag, bool ip_only)
+{
+
+}
+
+int main(int argc, char const* argv[])
+{
+    
+    return 0;
+}
+
